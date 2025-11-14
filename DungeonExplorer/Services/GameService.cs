85b09618a58e30117f710a3aac48e97423840b59
Dex Melton <spinning.pc@gmail.com>
2025-11-14 09:01:54 -0500

    Movement commands and buttons

--- DungeonExplorer/Services/GameService.cs
+++ DungeonExplorer/Services/GameService.cs
@@ -5,12 +5,137 @@ namespace DungeonExplorer.Services
 {
     public class GameService
     {
-        //Place holder for the moment
         public Dictionary<string, Room> Rooms { get; } = new();
 
         public GameService()
         {
-            // TODO: initialize Rooms with dungeon layout
+            InitializeRooms();
+        }
+
+        private void InitializeRooms()
+        {
+            Rooms["Great Hall"] = new Room
+            {
+                Name = "Great Hall",
+                Exits = new Dictionary<string, string>
+                {
+                    ["west"] = "West Tower",
+                    ["east"] = "East Tower",
+                    ["south"] = "Dungeon"
+                }
+            };
+
+            Rooms["East Tower"] = new Room
+            {
+                Name = "East Tower",
+                Exits = new Dictionary<string, string>
+                {
+                    ["west"] = "Great Hall",
+                    ["south"] = "Barracks"
+                },
+                Item = "Shield"
+            };
+
+            Rooms["Barracks"] = new Room
+            {
+                Name = "Barracks",
+                Exits = new Dictionary<string, string>
+                {
+                    ["west"] = "Dungeon",
+                    ["north"] = "East Tower",
+                    ["south"] = "Library"
+                },
+                Item = "Armor"
+            };
+
+            Rooms["Library"] = new Room
+            {
+                Name = "Library",
+                Exits = new Dictionary<string, string>
+                {
+                    ["west"] = "Bedroom",
+                    ["north"] = "Barracks"
+                }
+            };
+
+            Rooms["Bedroom"] = new Room
+            {
+                Name = "Bedroom",
+                Exits = new Dictionary<string, string>
+                {
+                    ["west"] = "Stables",
+                    ["east"] = "Library",
+                    ["north"] = "Dungeon"
+                },
+                Item = "Sword"
+            };
+
+            Rooms["Stables"] = new Room
+            {
+                Name = "Stables",
+                Exits = new Dictionary<string, string>
+                {
+                    ["north"] = "Kitchen",
+                    ["east"] = "Bedroom"
+                },
+                Item = "Bow"
+            };
+
+            Rooms["Kitchen"] = new Room
+            {
+                Name = "Kitchen",
+                Exits = new Dictionary<string, string>
+                {
+                    ["north"] = "West Tower",
+                    ["south"] = "Stables",
+                    ["east"] = "Dungeon"
+                },
+                Item = "Torch"
+            };
+
+            Rooms["West Tower"] = new Room
+            {
+                Name = "West Tower",
+                Exits = new Dictionary<string, string>
+                {
+                    ["south"] = "Kitchen",
+                    ["east"] = "Great Hall"
+                },
+                Item = "Arrows"
+            };
+
+            Rooms["Dungeon"] = new Room
+            {
+                Name = "Dungeon",
+                Exits = new Dictionary<string, string>
+                {
+                    ["north"] = "Great Hall",
+                    ["east"] = "Barracks",
+                    ["south"] = "Bedroom",
+                    ["west"] = "Kitchen"
+                }
+            };
+        }
+
+        public bool TryMove(Player player, string direction, out string message)
+        {
+            if (!Rooms.TryGetValue(player.CurrentRoom, out var currentRoom))
+            {
+                message = "You are in an unknown area.";
+                return false;
+            }
+
+            direction = direction.ToLowerInvariant();
+
+            if (currentRoom.Exits.TryGetValue(direction, out var nextRoomName))
+            {
+                player.CurrentRoom = nextRoomName;
+                message = $"You move {direction} to the {nextRoomName}.";
+                return true;
+            }
+
+            message = "You can't go that way.";
+            return false;
         }
     }
 }
--- DungeonExplorer/ViewModels/GameViewModel.cs
+++ DungeonExplorer/ViewModels/GameViewModel.cs
@@ -1,10 +1,14 @@
-﻿using DungeonExplorer.Models;
+﻿using System.Windows.Input;
+using DungeonExplorer.Models;
+using DungeonExplorer.Services;
 
 namespace DungeonExplorer.ViewModels
 {
     public class GameViewModel : BaseViewModel
     {
+        private readonly GameService _gameService;
         private Player _player;
+        private string _statusMessage = string.Empty;
 
         public Player Player
         {
@@ -12,17 +16,45 @@ namespace DungeonExplorer.ViewModels
             set => SetProperty(ref _player, value);
         }
 
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set => SetProperty(ref _statusMessage, value);
+        }
+
+        // Movement commands
+        public ICommand MoveNorthCommand { get; }
+        public ICommand MoveSouthCommand { get; }
+        public ICommand MoveEastCommand { get; }
+        public ICommand MoveWestCommand { get; }
+
         public GameViewModel()
         {
+            _gameService = new GameService();
+
             Player = new Player
             {
                 Name = "Dex the Brave",
                 CurrentRoom = "Great Hall"
             };
 
-            Player.Inventory.Add("Torch");
-            Player.Inventory.Add("Shield");
+            StatusMessage = "Your quest begins in the Great Hall...";
+
+            MoveNorthCommand = new RelayCommand(_ => Move("north"));
+            MoveSouthCommand = new RelayCommand(_ => Move("south"));
+            MoveEastCommand = new RelayCommand(_ => Move("east"));
+            MoveWestCommand = new RelayCommand(_ => Move("west"));
         }
 
+        private void Move(string direction)
+        {
+            if (_gameService.TryMove(Player, direction, out var message))
+            {
+                // Notify bindings that Player (and its properties) changed
+                OnPropertyChanged(nameof(Player));
+            }
+
+            StatusMessage = message;
+        }
     }
 }
